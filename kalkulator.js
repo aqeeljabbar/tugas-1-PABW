7abecd371a0a3d7ba2621084177b2e7a5c1e651c
Muhammad Aqeel Jabbar <aqeeljabbr19@gmail.com>
2024-03-18 16:02:20 +0700

    V.1 Operasi Pengurangan

--- kalkulator.js
+++ kalkulator.js
@@ -11,6 +11,9 @@ function hitung() {
         case "+":
             hasil = angka1 + angka2;
             break;
+        case "-":
+            hasil = angka1 - angka2;
+            break;
 
     }
 
