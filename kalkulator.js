c19eaa474dfd5229ee50d94f1ff437f2d0e79d69
Muhammad Aqeel Jabbar <aqeeljabbr19@gmail.com>
2024-03-18 16:21:43 +0700

    V.4 Operasi Modulus

--- kalkulator.js
+++ kalkulator.js
@@ -20,6 +20,9 @@ function hitung() {
         case "/":
             hasil = angka1 / angka2;
             break;
+        case "%":
+            hasil = angka1 % angka2;
+            break;
 
     }
 
