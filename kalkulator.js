f0027d00c98925b8927d8e4a9f58d0fb1bf8b7bd
Muhammad Aqeel Jabbar <aqeeljabbr19@gmail.com>
2024-03-18 12:00:10 +0700

    V.0 Operasi Penjumlahan

---
+++ kalkulator.js
@@ -0,0 +1,20 @@
+function hitung() {
+    let angka1 = document.querySelector("#angka1").value;
+    let angka2 = document.querySelector("#angka2").value;
+    let operasi = document.querySelector("#operasi").value;
+    let hasil;
+
+    angka1 = parseInt(angka1);
+    angka2 = parseInt(angka2);
+
+    switch (operasi) {
+        case "+":
+            hasil = angka1 + angka2;
+            break;
+
+    }
+
+    document.querySelector("#hasil").textContent = "Hasil: " + hasil;
+}
+
+document.querySelector("#hitung").addEventListener("click", hitung);
